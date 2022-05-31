fcdf9b52bd68a90689dfea6ca52b9db86c1f5927
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-05-31 19:54:30 -0300

    alterado kNN para funcionar com n-dimensoes | ajustado formatacao do arquivo

--- TDE1_kNN/main.py
+++ TDE1_kNN/main.py
@@ -4,12 +4,14 @@ import random
 
 SIZE = 20
 
-# elements thath will be predicted or
+
+# elements that will be predicted or
 # is in dataset
 class Element:
     def __init__(self, label: str, pos: tuple):
         self.label = label
         self.pos = pos
+
 
 # dataset with all elements
 class Dataset:
@@ -24,17 +26,28 @@ class Dataset:
             matriz[element.pos[0]][element.pos[1]] = '--' + element.label + '--'
         return matriz
 
+
 # eleva o número ao quadrado
 sqr = lambda x: x**2
 
 # raiz quadrada de um número
 sqrt = lambda x: x**0.5
 
+
 # distancia euclidian
-# - recebe duas tuplas (X,Y) e
+# - recebe duas tuplas (X, Y, .., Z) e
 # - retorna a distância euclidiana entre elas
 def distancia_euclidiana(p: tuple, q: tuple):
-    return sqrt(sqr(p[0]-q[0]) + sqr(p[1]-q[1]))
+    # as duas tuplas devem ter o mesmo número de atributos para que seja calculada corretamente
+    if len(p) != len(q):
+        return 0
+    somatorio = 0
+    index = 0
+    while len(p) > index:
+        somatorio += (sqr(p[index]-q[index]) + sqr(p[index]-q[index]))
+        index += 1
+    return sqrt(somatorio)
+
 
 # retorna os k menores elementos da lista
 # a lista contem elementos (str, float)
@@ -43,6 +56,7 @@ def menores_da_lista(k_menores: int, distancias: list):
     menores = distancias[:k_menores]
     return menores
 
+
 # conta as instancias da label na lista
 def conta_instancias(label: str, lst: list):
     contador = 0
@@ -50,6 +64,7 @@ def conta_instancias(label: str, lst: list):
         if label == lst[0]:
             contador+=1
     return contador
+
 
 # retorna a label de maior ocorrencia
 def maior_ocorrencia(lst: list):
@@ -61,31 +76,34 @@ def maior_ocorrencia(lst: list):
             ocorrencias.append(conta_instancias(e[0],lst))
     return labels[ocorrencias.index(max(ocorrencias))]
 
+
 # prediz label de element de acordo com k vizinhos próximos do dataset
 # - k: numero de vizinhos
 # - dataset: dataset com instancias
 # - element: elemento que sera predito
 # - f_distance: funcao que calcula a distancia entre 2 pontos
-def k_nearest_neghbors(k: int, dataset: Dataset, element: Element, f_distance = distancia_euclidiana):
+def k_nearest_neghbors(k: int, dataset: Dataset, element: Element, f_distance=distancia_euclidiana):
 #    distancias = []
 #    for e in dataset.ds:
 #        distancias.append((e.label,f_distance(element.pos,e.pos)))
 # gera uma tupla (label,distancia)
-    distancias = [(e.label,f_distance(element.pos,e.pos)) for e in dataset.ds]
+    distancias = [(e.label, f_distance(element.pos, e.pos)) for e in dataset.ds]
     return maior_ocorrencia(menores_da_lista(k, distancias))
 
+
 # populando o Dataset
-def popular_dataset(labels: list, dataset: Dataset, X = [i for i in range(SIZE)], Y = [i for i in range(SIZE)]):
+def popular_dataset(labels: list, ds: Dataset, X = [i for i in range(SIZE)], Y = [i for i in range(SIZE)]):
     for label in labels:
         random.shuffle(X)
         random.shuffle(Y)
         for i in range(10):
-            dataset.add_element(Element(label,(X[i],Y[i])))
+            ds.add_element(Element(label, (X[i], Y[i])))
+
 
 X = [i for i in range(SIZE)]
 Y = [i for i in range(SIZE)]
 dataset = Dataset()
-popular_dataset(['alpha','beta','teta','omega'],dataset,X,Y)
+popular_dataset(['alpha', 'beta', 'teta', 'omega'], dataset, X, Y)
 # gerando um novo elemento aleatorio
 random.shuffle(X)
 random.shuffle(Y)
